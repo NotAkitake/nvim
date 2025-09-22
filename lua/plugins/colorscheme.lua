6b30ba1faacc5b27dd32e3d764551f514fc96f0b
Akitake <akitake@pm.me>
2025-09-22 13:09:11 +0200

    Unpin catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,5 @@
 return {
   "catppuccin/nvim",
-  tag = "v1.10.0",
   lazy = true,
   name = "catppuccin",
   opts = {
@@ -9,20 +8,7 @@ return {
       light = "frappe",
       dark = "macchiato",
     },
-    transparent_background = false,
-    term_colors = true,
     default_integrations = true,
     auto_integrations = true,
-  },
-  specs = {
-    {
-      "akinsho/bufferline.nvim",
-      optional = true,
-      opts = function(_, opts)
-        if (vim.g.colors_name or ""):find("catppuccin") then
-          opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-        end
-      end,
-    },
   },
 }
